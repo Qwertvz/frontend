3a6143542c1baf3ed51ee22923a010186399369a
Qwertyvz <stalker-stalkerovich@list.ru>
2024-02-06 01:41:13 +0300

    Added main files

---
+++ pages/_document.tsx
@@ -0,0 +1,11 @@
+import { Html, Head, Main, NextScript } from "next/document"
+
+export default function Document() {
+  return <Html lang="en">
+        <Head />
+        <body className="bg-gray-500">
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+}
